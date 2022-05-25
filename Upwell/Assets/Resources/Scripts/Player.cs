2a7138b0ede07308f98e8e74c7ee37554aa428dd
Jonah <34112011+jcortezzo@users.noreply.github.com>
2022-05-24 20:45:16 -0700

    Initialize project

---
+++ Upwell/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Attributes")]
+    [SerializeField]
+    private float FLAP_FORCE;
+    private float MAX_SPEED;
+
+    private Vector3 mousePosition;
+
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Flap();
+        }
+    }
+
+    public void Flap()
+    {
+        Vector2 direction = this.transform.position - mousePosition;
+        rb.velocity = Vector2.zero;
+        rb.AddForce(direction.normalized * FLAP_FORCE, ForceMode2D.Impulse);
+    }
+}
