329abbd321bd58bb3b58b85470bd01d4dac479b3
Jonah <34112011+jcortezzo@users.noreply.github.com>
2022-05-25 19:50:38 -0700

    Add player sprite / animation

--- Upwell/Assets/Resources/Scripts/Player.cs
+++ Upwell/Assets/Resources/Scripts/Player.cs
@@ -12,11 +12,13 @@ public class Player : MonoBehaviour
     private Vector3 mousePosition;
 
     private Rigidbody2D rb;
+    private Animator anim;
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,6 +33,7 @@ public class Player : MonoBehaviour
 
     public void Flap()
     {
+        anim.Play("Player_flap");
         Vector2 direction = this.transform.position - mousePosition;
         rb.velocity = Vector2.zero;
         rb.AddForce(direction.normalized * FLAP_FORCE, ForceMode2D.Impulse);
